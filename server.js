f217d4db68f75edd98b17d36b928439fde3c3532
Denis Nikitin <dinya.nikitin@gmail.com>
2022-02-15 17:45:36 +0300

    create

--- server.js
+++ server.js
@@ -1,59 +1,40 @@
 const http = require('http');
-const port = process.env.PORT || 7070;
 const Koa = require('koa');
 const koaBody = require('koa-body');
 const app = new Koa();
-const uuid = require('uuid');
-const tickets = [];
 
+const tickets = [];
 
 app.use(koaBody({
     urlencoded: true,
     multipart: true,
     }));
-
-    app.use(async (ctx, next) => {
-        const origin = ctx.request.get('Origin');
-        if (!origin) {
-        return await next();
-        }
-        const headers = { 'Access-Control-Allow-Origin': '*', };
-        if (ctx.request.method !== 'OPTIONS') {
-        ctx.response.set({...headers});
-        try {
-        return await next();
-        } catch (e) {
-        e.headers = {...e.headers, ...headers};
-        throw e;
-        }
-        }
-        if (ctx.request.get('Access-Control-Request-Method')) {
-        ctx.response.set({
-        ...headers,
-        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH',
-        });
-        if (ctx.request.get('Access-Control-Request-Headers')) {
-        ctx.response.set('Access-Control-Allow-Headers', ctx.request.get('Access-Control-Allow-Request-Headers'));
-        }
-    ctx.response.status = 204; 
-    await next();
-    const { method } = ctx.request.querystring;
-
-    switch (method) {
-        case 'allTickets':
-            if(tickets){
-            ctx.response.body = tickets;
-            return;}
-            ctx.response.body = 'null';
+    
+    app.use(async ctx => {
         
-        default:
-            ctx.response.status = 404;
-            return;
-    }
-    }
-    });
+        ctx.response.set({
+            'Access-Control-Allow-Origin': '*',
+            });
+            
+         const { method } = ctx.request.query;
+        
+         switch(method) {
+            case 'allTickets' :
+                ctx.response.body = tickets;
+                return;
+            case 'createTicket' :
+                tickets.push(ctx.request.body);
+                ctx.response.body = tickets                        
+                return;
+            default:
+                    ctx.response.status = 404;
+                    
+                    return;
+         }
+        });
+        
+
     
 
+const server = http.createServer(app.callback()).listen(7070);
 
-        
-const server = http.createServer(app.callback()).listen(port);
