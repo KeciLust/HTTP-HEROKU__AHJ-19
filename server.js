f6ed0e058c0dd99135f793031c791f706ce3f646
Denis Nikitin <dinya.nikitin@gmail.com>
2022-02-17 22:27:28 +0300

    first finish

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const http = require('http');
 const Koa = require('koa');
 const koaBody = require('koa-body');
 const app = new Koa();
+const { v4: uuidv4 } = require('uuid');
+const port = process.env.PORT || 7070;
+
 
 const tickets = [];
 
@@ -11,21 +14,51 @@ app.use(koaBody({
     }));
     
     app.use(async ctx => {
-        
         ctx.response.set({
             'Access-Control-Allow-Origin': '*',
             });
             
-         const { method } = ctx.request.query;
+         const { method, id } = ctx.request.query;
         
          switch(method) {
             case 'allTickets' :
                 ctx.response.body = tickets;
                 return;
             case 'createTicket' :
-                tickets.push(ctx.request.body);
-                ctx.response.body = tickets                        
+                const ticket = JSON.parse(ctx.request.body);
+                ticket.id = uuidv4();
+                tickets.push(JSON.stringify(ticket));
+                ctx.response.body = {id: ticket.id};
+                                       
                 return;
+             case 'ticketById' :
+                     tickets.forEach((el) => {
+                     const e = JSON.parse(el)
+                     if(e.id === id){
+                        tickets.splice(tickets.indexOf(el));
+                         ctx.response.body = tickets;
+                         
+                     }
+                 });
+                 return;  
+                 case 'repearTicket':
+                    
+                    const repearTicket = JSON.parse(ctx.request.body);
+                    tickets.forEach((el) => {
+                        const e = JSON.parse(el)
+                      
+                        if(e.id === repearTicket.id){
+                           e.name = repearTicket.name;
+                           e.description = repearTicket.description;
+                           e.create = repearTicket.create;
+                           const er = JSON.stringify(e);
+                           tickets.splice(tickets.indexOf(el), 1, er);
+                           
+                          
+                           ctx.response.body = tickets;
+                        }
+                    });
+                    return;
             default:
                     ctx.response.status = 404;
                     
@@ -36,5 +69,5 @@ app.use(koaBody({
 
     
 
-const server = http.createServer(app.callback()).listen(7070);
+const server = http.createServer(app.callback()).listen(port);
 
